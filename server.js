04c1daadcd97c62c7d4550c609f8eebd1e7dff6b
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-12-15 22:44:17 -0500

    have manifest completed, working through service worker file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -15,13 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect("mongodb://localhost/budget");
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./controllers/api"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
